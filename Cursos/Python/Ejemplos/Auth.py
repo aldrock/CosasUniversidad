131bcb02d0900a26de2f3e7d888f7c0948d14b83
Rock Neurotiko <miguelglafuente@gmail.com>
2014-04-29 15:02:27 +0200

    Auth changed

--- Cursos/Python/Ejemplos/Auth.py
+++ Cursos/Python/Ejemplos/Auth.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-import pdb
-
 def auth(username):
 	if len(username) < 6:
 		print("El nombre de usuario debe contener al menos 6 caracteres")
@@ -18,7 +16,6 @@ def pwd(passwd):
 		print("La contraseña elegida no es segura")
 		return False
 	caps, low, num, noalpha = False, False, False, False
-	pdb.set_trace()
 	for i in passwd:
 		if not i.isalnum():
 			noalpha = True
